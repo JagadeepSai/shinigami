7398b8d8e5f1d398002222e68f46d8623e7cc11e
N JAGADEEP <jagadeep@cse.iitb.ac.in>
2017-10-01 20:17:28 +0530

    Sucessffully Implemented the skin ( neon-Ui )

--- core/src/com/mygdx/game/Screens/MainScreen.java
+++ core/src/com/mygdx/game/Screens/MainScreen.java
@@ -1,10 +1,12 @@
 package com.mygdx.game.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -17,22 +19,23 @@ import com.mygdx.game.MainClass;
  */
 
 public class MainScreen extends ScreenAdapter {
-  
+
     MainClass game;
     OrthographicCamera cam;
-
-    private Stage stage;
+    Stage stage;
     private Skin skin;
+    private Texture badlogic;
 
 
     public MainScreen(MainClass game){
     this.game = game;
-    }
-
-    public void create(){
-        skin = new Skin(Gdx.files.internal("neon-ui.jsson"));
-        cam = new OrthographicCamera();
+        skin = new Skin(Gdx.files.internal("neon-ui.json"));
+       // skin.add("text");
+        cam = new OrthographicCamera(800,400);
+        game.batch = new SpriteBatch();
         stage = new Stage(new ScreenViewport());
+
+        badlogic = new Texture("badlogic.jpg");
 
         final TextButton button = new TextButton("Click me ",skin,"default");
         button.setWidth(200);
@@ -42,11 +45,40 @@ public class MainScreen extends ScreenAdapter {
         Gdx.input.setInputProcessor(stage);
     }
 
+
+    @Override
+    public void show() {
+
+    }
+
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act(Gdx.graphics.getDeltaTime());
+       // game.batch.begin();
+       // game.batch.draw(badlogic,0,0);
+       // game.batch.end();
+         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
     }
 
     @Override
