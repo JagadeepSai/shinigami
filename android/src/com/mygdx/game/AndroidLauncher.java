0bc5812fa46b273c328b18f5fe6acdf2df58a123
SURAJ SONI <surajsoni@cse.iitb.ac.in>
2017-10-02 02:04:01 +0530

    Succesfully added firebase

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -4,9 +4,10 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.MainClass;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AndroidLauncher extends AndroidApplication {
+	//private FirebaseAuth mAuth;
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
