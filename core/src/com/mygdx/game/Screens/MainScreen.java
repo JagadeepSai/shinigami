92b3b39aaae7e2f5a37f8ef011e0c9a600e10298
N JAGADEEP <jagadeep@cse.iitb.ac.in>
2017-10-01 18:39:01 +0530

    Corrected neon-ui JSON file

---
+++ core/src/com/mygdx/game/Screens/MainScreen.java
@@ -0,0 +1,14 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+/**
+ * Created by root on 1/10/17.
+ */
+
+public class MainScreen extends ScreenAdapter {
+
+    public void MainScreen(){
+
+    }
+}
