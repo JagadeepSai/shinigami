971be3835cb950203a6db429df0daf2ea6143bd4
N JAGADEEP <jagadeep@cse.iitb.ac.in>
2017-10-02 15:39:58 +0530

    Unable to change the font to TextButton Created the GameScreen

---
+++ core/src/com/mygdx/game/Screens/GameScreen.java
@@ -0,0 +1,14 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+import com.mygdx.game.MainClass;
+
+/**
+ * Created by root on 2/10/17.
+ */
+
+class GameScreen extends ScreenAdapter{
+    MainClass game;
+
+
+}
