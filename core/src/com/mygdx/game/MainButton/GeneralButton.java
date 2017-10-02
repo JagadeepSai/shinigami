39a332d347d1ca911a460878886f3d3d19b0d5ae
N JAGADEEP <jagadeep@cse.iitb.ac.in>
2017-10-02 22:51:00 +0530

    Main Menu Screen Completed  -- Background remaining   -Added All the Icons needed for the Main Menu Screen   -Created General Button class to simplify the code for making a Imagebutton

---
+++ core/src/com/mygdx/game/MainButton/GeneralButton.java
@@ -0,0 +1,81 @@
+package com.mygdx.game.MainButton;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.tools.flame.EventManager;
+
+/**
+ * Created by root on 2/10/17.
+ */
+
+public class GeneralButton  {
+
+   public ImageButton button ;
+    ImageButtonStyle buttonStyle;
+
+    public GeneralButton(String buttonup, String buttondown ) {
+
+        if(buttondown == "") buttondown = buttonup;
+        buttonStyle = new ImageButtonStyle();
+        buttonStyle.up = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal(buttonup))));
+        buttonStyle.down = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal(buttondown))));
+        button = new ImageButton(buttonStyle);
+    }
+
+    public void setHover(String buttonover){
+        buttonStyle.over = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal(buttonover))));
+        button.setStyle(buttonStyle);
+    }
+
+    public void setWidth(float width){
+        button.setWidth(width);
+    }
+
+    public void setHeight(float height){
+        button.setHeight(height);
+    }
+
+    public void setPosition(float x , float y){
+        button.setPosition(x,y);
+    }
+
+    public float getWidth(){
+        return  button.getWidth();
+    }
+
+    public float getHeight(){
+        return button.getHeight();
+    }
+
+    public void setTouchable(){
+        button.setTouchable(Touchable.enabled);
+    }
+
+    public void addListener(ChangeListener listener){
+        button.addListener(listener);
+    }
+
+    public  void setOrigin(float  x ,float y){
+        button.setOrigin(x,y);
+    }
+
+    public void rotate(float x){
+        button.rotateBy(x);
+    }
+
+    public void addAction(Action action){
+        button.addAction(action);
+    }
+
+
+}
--- core/src/com/mygdx/game/Screens/MainScreen.java
+++ core/src/com/mygdx/game/Screens/MainScreen.java
@@ -11,29 +11,40 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.actions.RepeatAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.kotcrab.vis.ui.i18n.BundleText;
+import com.mygdx.game.MainButton.GeneralButton;
 import com.mygdx.game.MainClass;
 
 import javax.swing.text.View;
 
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.repeat;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.rotateBy;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.rotateTo;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
 import static java.awt.SystemColor.info;
 
 /**
@@ -44,7 +55,10 @@ public class MainScreen extends ScreenAdapter {
 
     MainClass game;
     Stage stage;
-    final TextButton button ;
+    GeneralButton playbutton ;
+    GeneralButton settingbutton ;
+    GeneralButton groupbutton;
+    GeneralButton createbutton;
     Texture background = new Texture("background.png");
 
    // Type font
@@ -62,19 +76,21 @@ public class MainScreen extends ScreenAdapter {
     private Skin skin;
 
     public MainScreen(MainClass game){
-
         this.game = game;
+        /// Font for the Skin ( Failed )
+    /*
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("font/Play.TTF"));
-        FreeTypeFontParameter parametr = new FreeTypeFontParameter();
+          FreeTypeFontParameter parametr = new FreeTypeFontParameter();
         parametr.size = 12 ;
         BitmapFont playfont =  generator.generateFont(parametr);
-//        BitmapFontWriter.writeFont(data, new String[] {"font.png"},Gdx.files.absolute("font.fnt"), info, 512, 512);
-//        BitmapFontWriter.writePixmaps(param.packer.getPages(), Gdx.files.absolute("imageDir"), name);
+        BitmapFontWriter.writeFont(data, new String[] {"font.png"},Gdx.files.absolute("font.fnt"), info, 512, 512);
+        BitmapFontWriter.writePixmaps(param.packer.getPages(), Gdx.files.absolute("imageDir"), name);
 
         generator.dispose();
+    */
+    /// Skin for the buttons
 
-
-        skin = new Skin(Gdx.files.internal("neon-ui.json"));
+     /*   skin = new Skin(Gdx.files.internal("neon-ui.json"));
         skin.add("font", playfont, BitmapFont.class);
         FileHandle filehandle = Gdx.files.internal("neon-ui.json");
         FileHandle atlasfile = filehandle.sibling("neon-ui.json") ;
@@ -82,13 +98,10 @@ public class MainScreen extends ScreenAdapter {
             skin.addRegions(new TextureAtlas(atlasfile));
         }
         skin.load(filehandle);
-//        skin.addRegions(new TextureAtlas(Gdx.files.internal("neon-ui.atlas")));
-//        skin.load(Gdx.files.internal("neon-ui.json"));
+        skin.addRegions(new TextureAtlas(Gdx.files.internal("neon-ui.atlas")));
+        skin.load(Gdx.files.internal("neon-ui.json"));
 
-        viewport = new StretchViewport(GameWidth ,GameWidth/AspectRatio1);
-        stage = new Stage(viewport);
-
-        button = new TextButton("PLAY",skin,"default");
+                button = new TextButton("PLAY",skin,"default");
         button.getStyle().font = playfont;
         button.setWidth((150/310.5f)*GameWidth);
         button.setHeight(button.getWidth()*AspectRatio1/2f);
@@ -96,10 +109,39 @@ public class MainScreen extends ScreenAdapter {
        // TextButton.TextButtonStyle tbs = new TextButton.TextButtonStyle();
        /// tbs.font= playfont;
 
+         stage.addActor(button);
 
-        stage.addActor(button);
+*/
+        playbutton = new GeneralButton("icons/Circled Play-260.png","");
+        playbutton.setWidth(GameWidth/3);
+        playbutton.setHeight(playbutton.getWidth()/AspectRatio);
+        playbutton.setPosition(GameWidth/2 - playbutton.getWidth()/2 ,GameWidth/(AspectRatio*2) + playbutton.getHeight());
 
-        //stage.addActor(button);
+        settingbutton = new GeneralButton("icons/Settings-500.png","");
+        settingbutton.setWidth(GameWidth/6);
+        settingbutton.setHeight(settingbutton.getWidth()/AspectRatio);
+        settingbutton.setPosition(GameWidth/5 - settingbutton.getWidth()/2 , GameHeight/(AspectRatio*6) - settingbutton.getHeight()/2 );
+        settingbutton.setOrigin(settingbutton.getWidth()/2,settingbutton.getWidth()/2);
+       // settingbutton.addAction(Actions.rotateBy(360,1));
+
+        groupbutton = new GeneralButton("icons/User Groups-260.png","");
+        groupbutton.setWidth(GameWidth/5.3f);
+        groupbutton.setHeight(groupbutton.getWidth()/AspectRatio);
+        groupbutton.setPosition(3.9f*GameWidth/5 - groupbutton.getWidth()/2 , GameHeight/(AspectRatio*6) - groupbutton.getHeight()/2 );
+
+        createbutton = new GeneralButton("icons/Create -260.png","");
+        createbutton.setWidth(GameWidth/4.5f);
+        createbutton.setHeight(createbutton.getWidth()/AspectRatio);
+        createbutton.setPosition(GameWidth/2 - createbutton.getWidth()/2,GameWidth/(AspectRatio*2) - createbutton.getHeight()/2);
+
+        viewport = new StretchViewport(GameWidth ,GameWidth/AspectRatio1);
+        stage = new Stage(viewport);
+
+
+        stage.addActor(playbutton.button);
+        stage.addActor(settingbutton.button);
+        stage.addActor(groupbutton.button);
+        stage.addActor(createbutton.button);
         Gdx.input.setInputProcessor(stage);
     }
 
@@ -112,20 +154,22 @@ public class MainScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
-        button.setTouchable(Touchable.enabled);
-        button.addListener(new ChangeListener() {
+        playbutton.setTouchable();
+        playbutton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 game.setScreen(new GameScreen(game));
             }
         });
+        settingbutton.addAction(repeat(RepeatAction.FOREVER,sequence(
+                rotateBy(360,1),rotateTo(0))));
 
         stage.act(Gdx.graphics.getDeltaTime());
 
         Gdx.gl.glClearColor(1,1,1,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.getBatch().begin();
-        stage.getBatch().draw(background,0,0,GameWidth,GameHeight);
+      //  stage.getBatch().draw(background,0,0,GameWidth,GameHeight);
         stage.getBatch().end();
         stage.draw();
     }
