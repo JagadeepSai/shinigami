43581546ffa9a038b90f486d8479561891cdc83a
N JAGADEEP <jagadeep@cse.iitb.ac.in>
2017-10-11 15:23:10 +0530

    Scroll and Tap of the Obstacles implemented.
    TODO : Adding of buttons below .
    Pinch to zoom Or buttons for zooming .

--- core/src/com/mygdx/game/Screens/StageCreatorScreen.java
+++ core/src/com/mygdx/game/Screens/StageCreatorScreen.java
@@ -2,12 +2,22 @@ package com.mygdx.game.Screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
+import com.mygdx.game.MainButton.BallButton;
 import com.mygdx.game.MainButton.GeneralButton;
 import com.mygdx.game.MainClass;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by root on 8/10/17.
@@ -16,21 +26,69 @@ import com.mygdx.game.MainClass;
 public class StageCreatorScreen extends ScreenAdapter {
 
     MainClass game ;
-    GeneralButton ontapcircle;
-    Vector<Vector2> positions ;
-    Vector2 touch = new Vector2();
+    List<BallButton> obs ;
+    int total_count = 0;
 
-    public  StageCreatorScreen (MainClass gam){
+    Image back;
+
+    public float GameWidth = Gdx.graphics.getWidth();
+    public float GameHeight = Gdx.graphics.getHeight();
+    public float AspectRatio = (float)(Gdx.graphics.getHeight())/(float)(Gdx.graphics.getWidth());
+    public float AspectRatio1 = 16/9;
+
+
+    Vector2 touch = new Vector2();
+    private Vector3 prevDragPos;
+
+    public  StageCreatorScreen (MainClass gam) {
         this.game = gam;
-        ontapcircle = new GeneralButton("icons/Mesh-260.png","");
+        obs = new ArrayList<BallButton>();
+        game.stage.addListener(new ActorGestureListener() {
+
+            @Override
+            public void pan(InputEvent event, float x, float y, float deltaX, float deltaY) {
+                if(y>GameWidth/4) {
+                    game.stage.getCamera().translate(0, -deltaY, 0);
+                    game.stage.getCamera().update();
+                    //System.out.println(x + ", " + y + ", " + "      " + deltaX + ", " + deltaY);
+
+                }
+            }
+            
+
+            @Override
+            public void  tap(InputEvent event, float x, float y, int count, int button) {
+                obs.add(new BallButton(GameWidth/3,GameWidth/(3*AspectRatio)));
+                obs.get(total_count).setPosition(x,y);
+                //  obs.get(total_count).setPosition(Gdx.input.getX(),GameHeight- Gdx.input.getY());
+                // obs.setPosition(positions.get(count).x,positions.get(count).y)
+
+                game.stage.addActor(obs.get(total_count).ontapbutton.button) ;
+                total_count++;
+            }
+        });
+
+        Gdx.input.setInputProcessor(game.stage);
+
+        back = new Image(new Texture(Gdx.files.internal("background.png")));
+        game.stage.addActor(back);
+
     }
+
 
     @Override
     public void render(float delta) {
-        if(Gdx.input.justTouched()) {
+        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        /*if(Gdx.input.justTouched()) {
+            obs.add(new BallButton(GameWidth/3,GameWidth/(3*AspectRatio)));
+            obs.get(count).setPosition(Gdx.input.getX(),GameHeight- Gdx.input.getY());
+           // obs.setPosition(positions.get(count).x,positions.get(count).y)
 
-            touch.set(Gdx.input.getX(), Gdx.input.getY());
-        }
+            game.stage.addActor(obs.get(count).ontapbutton.button) ;
+            count++;
+        }*/
+        game.stage.draw();
         super.render(delta);
     }
 
--- core/src/com/mygdx/game/Screens/UserGameScreen.java
+++ core/src/com/mygdx/game/Screens/UserGameScreen.java
@@ -3,6 +3,8 @@ package com.mygdx.game.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.MainButton.GeneralButton;
 import com.mygdx.game.MainClass;
 
@@ -38,6 +40,16 @@ public class UserGameScreen extends ScreenAdapter {
     public void render(float delta) {
         Gdx.gl.glClearColor(1,1,1,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        designbutton.setTouchable();
+        designbutton.button.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                game.getScreen().hide();
+                game.stage.clear();
+                StageCreatorScreen stageCreatorScreen = new StageCreatorScreen(game);
+                game.setScreen(stageCreatorScreen);
+            }
+        });
 
         game.stage.draw();
     }
