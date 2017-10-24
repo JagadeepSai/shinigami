ba61858348ae13cbdc061437264a8d37e20a0b61
SURAJ SONI <surajsoni@cse.iitb.ac.in>
2017-10-24 19:39:09 +0530

    Tested file manager

--- core/src/com/mygdx/game/MainClass.java
+++ core/src/com/mygdx/game/MainClass.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -12,6 +13,8 @@ import com.mygdx.game.Screens.MainScreen;
 import com.mygdx.game.Screens.Transitions.TransitionScreen;
 import com.mygdx.game.Screens.UserGameScreen;
 
+import java.io.OutputStream;
+
 public class MainClass extends Game {
 	public Stage stage;
 	public Viewport viewport ;
@@ -19,6 +22,13 @@ public class MainClass extends Game {
 	public Assets assets;
 	@Override
 	public void create () {
+		Gdx.app.log("qwer",Gdx.files.getExternalStoragePath());
+		FileHandle fileHandle;
+		fileHandle=Gdx.files.local("test.txt");
+		fileHandle.writeString("\n1iiwiiuwiuiw",true);
+
+
+		Gdx.app.log("1234",fileHandle.readString());
         stage = new Stage();
 		viewport = new StretchViewport(0,0);
 		//UserGameScreen userGameScreen = new UserGameScreen(this);
